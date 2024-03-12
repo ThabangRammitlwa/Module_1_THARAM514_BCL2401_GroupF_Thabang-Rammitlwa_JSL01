dce3780c00df00e6bf255d42feab523baebf746b
Thabang Rammitlwa <thabang451@gmail.com>
2024-03-12 11:02:30 +0200

    complete

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let petInput = document.getElementById('petInput').value.trim();
+    let regex = /^pet_[a-zA-Z0-9]+$/;
+    let isValid=regex.test(petInput)
+    let resultElement=document.getElementById ('result'); 
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    
+    if (isValid) {
+        resultElement.innerHTML = "Valid syntax." + '<span style="color: green; font-size: 50px;">&#x25cf;</span>';
+    } else {
+        resultElement.innerHTML = "Invalid syntax." + '<span style="color: red; font-size: 50px;">&#x25cf;</span>';
+    }
 
-            document.getElementById('result').innerText = result;
 }
 
 
